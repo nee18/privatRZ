9ddeda469993eabdd3d15c22f93cb26237d0ab65
nee18 <99226926+nee18@users.noreply.github.com>
2022-12-10 02:08:32 +0700

    fhola

--- src/scripts/views/templates/Item.js
+++ src/scripts/views/templates/Item.js
@@ -3,7 +3,7 @@ import API_ENDPOINT from '../../globals/api-endpoint';
 const postItem = (Item) => `
   <div id="list-item" class="card">
     <div class="card-header">
-      <img class="card-img lazyload" src="${API_ENDPOINT.RESTAURANT_IMAGE + Item.pictureId}" alt="${Item.name}" title="${Item.name}">
+      <img class="card-img lazyload" data-src="${API_ENDPOINT.RESTAURANT_IMAGE + Item.pictureId}" alt="${Item.name}" title="${Item.name}">
       <h5 class="card-kota">Kota ${Item.city}</h5>
     </div>
     <h4>Rating: ${Item.rating}</h4>
